3c1715a96b603dbb079d692752c501cb9b185d80
Srinithi <srinithinithiyanantham@gmail.com>
2026-09-06 21:47:44 +0530

    changed gmaill

--- backend/index.js
+++ backend/index.js
@@ -65,7 +65,7 @@ app.post("/sendEmail", async (req, res) => {
     console.log("Content:", content);
 
     const transporter = nodemailer.createTransport({
-        service: "gmail.com",
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD,
