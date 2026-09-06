496f7687e648d1a1fb2b189f6c992d558dd8d103
Srinithi <srinithinithiyanantham@gmail.com>
2026-09-06 17:42:25 +0530

    changing the route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter, Routes ,Route} from 'react-router-dom'
-import IndexPage from './components/indexPage.jsx'
+import IndexPage from './components/IndexPage.jsx'
 import History from './components/History.jsx'
 
 createRoot(document.getElementById('root')).render(
